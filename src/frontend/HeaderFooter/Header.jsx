a46ec5c990ff8cc05995ec2c805e83367136aafc
=techgtf <bhupendra.singh@gtftechnologies.com>
2025-01-28 12:56:56 +0530

    merged

--- src/frontend/HeaderFooter/Header.jsx
+++ src/frontend/HeaderFooter/Header.jsx
@@ -1,46 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import "./header.css"
-import { Link, useLocation } from 'react-router-dom'
-import { BASE_ROOT } from '../../../config'
-import * as CONFIG from '../../../config'
+import React, { useEffect, useState } from 'react';
+import "./header.css";
+import { Link } from 'react-router-dom';
+import { BASE_ROOT } from '../../../config';
+import * as CONFIG from '../../../config';
 
 export default function Header() {
-  const [isActive, setIsActive] = useState(false);
-  const location = useLocation();
-  const [aboutus, setAboutUsPage] = useState(false);
+  const [isFixed, setIsFixed] = useState(false);
+  const [prevScrollY, setPrevScrollY] = useState(0);
+
   useEffect(() => {
-    // if (location.pathname === '/about-us') setIsActive(true); setAboutUsPage(true)
     const handleScroll = () => {
-      if (window.scrollY >= 70) {
-        setIsActive(true);
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY === 0) {
+        setIsFixed(false);
       } else {
-        setIsActive(false);
+        if (currentScrollY < prevScrollY && currentScrollY >= 100) {
+          setIsFixed(true);
+        } else if (currentScrollY > prevScrollY) {
+          setIsFixed(false);
+        }
       }
+
+      setPrevScrollY(currentScrollY);
     };
+
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
-
-
-
-
+  }, [prevScrollY]);
 
   return (
-    <header className={`app_header fixed top-0 left-0 ring-0 w-full ${isActive ? "active" : ""} ${aboutus ? '!bg-transparent !border-b-[0px]' : ''}`}>
-      <div className='max-w-[90%] m-auto'>
+    <header className={`app_header ${isFixed ? "fixed active" : "relative"} top-0 left-0 ring-0 w-full !z-10`}>
+      <div className='max-w-[95%] m-auto'>
         <div className='flex justify-between'>
           <Link to={`${BASE_ROOT}`}>
             <img
-              className={`lg:h-[60px] ${isFixed ? "logo-colored" : "logo-white"} w-[40%]  sm:w-full`} 
+              className={`lg:h-[60px] ${isFixed ? "logo-colored" : "logo-white"} w-[40%]  sm:w-full`}
               src={`${CONFIG.ASSET_IMAGE_URL}frontend/images/logo${isFixed ? '-colored' : ''}.png`}
               alt="logo"
             />
           </Link>
           <button className='menuBtn'>
             <img
-              className={`lg:h-[35px] ${isFixed ? 'whiteIcon' : 'colredIcon'}`} 
+              className={`lg:h-[35px] ${isFixed ? 'whiteIcon' : 'colredIcon'}`}
               src={`${CONFIG.ASSET_IMAGE_URL}frontend/images/icons/menu${isFixed ? '1' : ''}.png`}
               alt='menu'
             />
@@ -48,5 +52,5 @@ export default function Header() {
         </div>
       </div>
     </header>
-  )
+  );
 }
