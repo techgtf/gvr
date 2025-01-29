a25b0b7e262039e81c66289c7666c8ac9b4cba63
vikas2489 <vk55002678@gmail.com>
2025-01-29 15:49:12 +0530

    update our journey section

--- src/frontend/components/aboutUs/OurVision.jsx
+++ src/frontend/components/aboutUs/OurVision.jsx
@@ -1,7 +1,7 @@
 const OurVision = () => {
   return (
     <div className="bg-[#33638B] my-[3.5rem]  text-white flex justify-center">
-      <div className="flex justify-between items-center py-[6.5rem] flex-wrap max-w-[85%]">
+      <div className="flex justify-between items-center py-[4.5rem] flex-wrap max-w-[85%]">
         <img
           className="w-[350px] h-[350px]"
           src="assets/frontend/images/aboutus/vision_and_mission.jpg"
@@ -12,7 +12,7 @@ const OurVision = () => {
             <h3 className="midlandfontmedium text-[11px] tracking-[2px]">
               Our Vision
             </h3>
-            <p className="text-[13px] mt-[1rem] poppins-regular">
+            <p className="text-[13px] mt-[1rem] poppins-regular font-[300]">
               Our vision is to set new industry standards by delivering
               unparalleled quality through continuous innovation. We aspire to
               create unique, customer-focused solutions that redefine
@@ -23,7 +23,7 @@ const OurVision = () => {
             <h3 className="midlandfontmedium text-[11px] tracking-[2px]">
               Our Mission
             </h3>
-            <p className="text-[13px] mt-[1rem] poppins-regular">
+            <p className="text-[13px] mt-[1rem] poppins-regular font-[300]">
               Our mission is to drive national progress and enrich lives by
               delivering world-class infrastructure and financial services. We
               are committed to building a future where innovation meets
--- src/frontend/components/overviewSection/overviewSection.jsx
+++ src/frontend/components/overviewSection/overviewSection.jsx
@@ -1,33 +1,37 @@
-import React from 'react'
-import './styles.css';
-import CommonHeading from '../commonHeading';
-import CommonPera from '../commonPera';
-import CommonBtn from '../commonBtn';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./styles.css";
+import CommonHeading from "../commonHeading";
+import CommonPera from "../commonPera";
+import CommonBtn from "../commonBtn";
+import { Link } from "react-router-dom";
 import { BsArrowUpRight } from "react-icons/bs";
-import { useTextAnimation } from '../useTextAnimation';
+import { useTextAnimation } from "../useTextAnimation";
 
+export default function OverviewSection({ heading, paragraph }) {
+  const sectionRef = useTextAnimation(
+    { from: { y: 100, opacity: 0 }, to: { y: 0, opacity: 1, duration: 1 } },
+    []
+  );
 
-
-export default function OverviewSection({heading,paragraph}) {
-
-    const sectionRef = useTextAnimation(
-        { from: { y: 100, opacity: 0 }, to: { y: 0, opacity: 1, duration: 1 } },
-        []
-    );
-
-    return (
-        <div className='overview_section'
-            // data-speed="clamp(.9)"
-            ref={sectionRef}
-        >
-            <div className='headingWrap max-w-[79%] m-auto text-center'>
-                <CommonHeading HeadingText={heading} />
-            </div>
-            <div className='content max-w-[85%] m-auto lg:mt-[50px] lg:mb-[50px] text-center'>
-                <CommonPera PeraclassName='fontItalic' PeraText='The genesis of Great Value Industries dates to 1970 when the group set up its glassware division. In 1990 GVIL diversified into together supplying quality packaging products to prestigious.' />
-            </div>
-            <CommonBtn to={""} TagName={Link} target='_blank'>Know More <BsArrowUpRight /> </CommonBtn>
-        </div>
-    )
+  return (
+    <div
+      className="overview_section"
+      // data-speed="clamp(.9)"
+      ref={sectionRef}
+    >
+      <div className="headingWrap max-w-[79%] m-auto text-center">
+        <CommonHeading HeadingText={heading} />
+      </div>
+      <div className="content max-w-[85%] m-auto lg:mt-[50px] lg:mb-[50px] text-center">
+        <CommonPera
+          PeraclassName="fontItalic"
+          //   PeraText="The genesis of Great Value Industries dates to 1970 when the group set up its glassware division. In 1990 GVIL diversified into together supplying quality packaging products to prestigious."
+          PeraText={paragraph}
+        />
+      </div>
+      <CommonBtn to={""} TagName={Link} target="_blank">
+        Know More <BsArrowUpRight />{" "}
+      </CommonBtn>
+    </div>
+  );
 }
