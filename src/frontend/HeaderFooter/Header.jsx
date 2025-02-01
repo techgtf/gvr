eb4f6e393bae6c7cd721eb79cd386d1f299d0e5f
khushi-gtf53 <khushi.kashyap@gtftechnologies.com>
2025-02-01 14:26:35 +0530

    add menu in header

--- src/frontend/HeaderFooter/Header.jsx
+++ src/frontend/HeaderFooter/Header.jsx
@@ -3,9 +3,14 @@ import "./header.css";
 import { Link, useLocation } from "react-router-dom";
 import { BASE_ROOT } from "../../../config";
 import * as CONFIG from "../../../config";
+import { IoChevronDown } from "react-icons/io5";
+import SideMenu from "./SideMenu";
+import NavDropdown from "./NavDropdown";
 
 export default function Header() {
   const [isFixed, setIsFixed] = useState(false);
+  const [openSidebar, setOpenSidebar] = useState(false)
+  const [dropdown, setDropdown] = useState(false)
   const [prevScrollY, setPrevScrollY] = useState(0);
   const location = useLocation();
   useEffect(() => {
@@ -30,47 +35,55 @@ export default function Header() {
       window.removeEventListener("scroll", handleScroll);
     };
   }, [prevScrollY]);
-  
+
   return (
     <header
-      className={`app_header ${
-        isFixed ? "fixed active" : "relative"
-      } top-0 left-0 ring-0 w-full !z-10`}
+      className={`app_header  ${isFixed ? "fixed active" : "relative"
+        } top-0 left-0 ring-0 w-full !z-10`}
     >
       <div className="max-w-[95%] m-auto">
-        <div className="flex justify-between">
+        <div className="flex justify-between items-center">
           <Link to={`${BASE_ROOT}`}>
             <img
-              className={`lg:h-[60px] ${
-                isFixed || location.pathname === `${BASE_ROOT}about-us`
-                  ? "logo-colored"
-                  : "logo-white"
-              } w-[40%]  sm:w-full`}
-              src={`${CONFIG.ASSET_IMAGE_URL}frontend/images/logo${
-                isFixed || location.pathname === `${BASE_ROOT}about-us`
-                  ? "-colored"
-                  : ""
-              }.png`}
+              // lg:h-[60px]
+              className={` ${isFixed || location.pathname === `${BASE_ROOT}about-us`
+                ? "logo-colored"
+                : "logo-white"
+                } w-[40%]  sm:w-[50%]`}
+              src={`${CONFIG.ASSET_IMAGE_URL}frontend/images/logo${isFixed || location.pathname === `${BASE_ROOT}about-us`
+                ? "-colored"
+                : ""
+                }.png`}
               alt="logo"
             />
           </Link>
-          <button className="menuBtn flex justify-end items-center">
-            <img
-              className={`lg:h-[35px] ${
-                isFixed || location.pathname === `${BASE_ROOT}about-us`
+          <div className="right_nav flex justify-between items-center gap-10">
+            <div className={`nav_items uppercase ${isFixed || location.pathname === `${BASE_ROOT}about-us` ? "text-black" : "text-white"}`}>
+              <ul className="flex justify-evenly gap-10 items-center">
+                <li  onClick={()=>setDropdown(true)} className="flex gap-3 items-center tracking-[4px] text-[14px] font-[200]">residential <IoChevronDown className="text-xl" /></li>
+                <li onClick={()=>setDropdown(true)} className="flex gap-3 items-center tracking-[4px] text-[14px] font-[200]">commercial <IoChevronDown className="text-xl" /></li>
+                <li onClick={()=>setDropdown(true)} className="flex gap-3 items-center tracking-[4px] text-[14px] font-[200]">esg <IoChevronDown className="text-xl" /></li>
+              </ul>
+            </div>
+            <button className="menuBtn flex justify-end items-center">
+              <img
+                className={`lg:h-[35px] cursor-pointer ${isFixed || location.pathname === `${BASE_ROOT}about-us`
                   ? "whiteIcon w-[30%]"
                   : "colredIcon w-[80%]"
-              }   sm:w-full`}
-              src={`${CONFIG.ASSET_IMAGE_URL}frontend/images/icons/menu${
-                isFixed || location.pathname === `${BASE_ROOT}about-us`
+                  }   sm:w-full`}
+                src={`${CONFIG.ASSET_IMAGE_URL}frontend/images/icons/menu${isFixed || location.pathname === `${BASE_ROOT}about-us`
                   ? "1"
                   : ""
-              }.png `}
-              alt="menu"
-            />
-          </button>
+                  }.png `}
+                alt="menu" onClick={()=>setOpenSidebar(true)}
+              />
+            </button>
+          </div>
         </div>
       </div>
+      {openSidebar ? <SideMenu setOpenSidebar={setOpenSidebar} /> : ""}
+      {dropdown ? <NavDropdown setDropdown={setDropdown} /> : ""}
+
     </header>
   );
 }
---
+++ src/frontend/HeaderFooter/NavDropdown.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react'
+import CommonHeading from '../components/commonHeading'
+import { Link } from 'react-router-dom'
+import { SlClose } from 'react-icons/sl'
+import gsap from 'gsap';
+
+function NavDropdown({setDropdown}) {
+
+    useEffect(() => {
+        gsap.fromTo(".nav_dropdown", 
+            {
+                y: 100,
+                opacity: 0
+            }, {
+                y: 0,
+                opacity: 1,
+                duration: 1,
+            });
+    }, []);
+
+    const handleClose = () => {
+        gsap.to(".nav_dropdown", {
+            y: 100,
+            opacity: 0,
+            duration: 1,
+            onComplete: () => {
+                setDropdown(false);  
+            }
+        });
+    };
+
+    return (
+        <>
+            <div className="nav_dropdown fixed top-[73px] left-0 h-[50vh] w-full bg-[#EFF5FA] text-black opacity-95 p-10">
+                <div className="relative h-full">
+                    <ul className='flex justify-evenly items-center h-full'>
+                        <li>
+                            <Link> <CommonHeading HeadingText="SHARANAM" />
+                                <p className="place uppercase pt-2 cursor-pointer">sector 107, noida</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link>
+                                <CommonHeading HeadingText="ANANDAM" />
+                                <p className="place uppercase pt-2 cursor-pointer">sector 107, noida</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link>
+                                <CommonHeading HeadingText="GV HOMEZ" />
+                                <p className="place uppercase pt-2 cursor-pointer">uday park, new delhi</p>
+                            </Link>
+                        </li>
+                    </ul>
+
+                    <div className="absolute bottom-0 left-[50%]">
+                        <SlClose onClick={handleClose} className="cursor-pointer text-3xl text-[#00000094]" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default NavDropdown
