a8b60c94285b91eeab2513848b66a112fe6e57c8
=techgtf <bhupendra.singh@gtftechnologies.com>
2025-02-03 17:08:27 +0530

    emi cal

--- src/frontend/pages/emiCalculator.jsx
+++ src/frontend/pages/emiCalculator.jsx
@@ -1,24 +1,16 @@
 import React from 'react'
 import Index from '../components/emiCalculator'
 import * as CONFIG from "../../../config";
+import HeroSectionAboutUs from '../components/aboutUs/HeroSectionAboutUs';
 
 export default function miECalculator() {
     return (
         <>
-            <div className="relative">
-                <img
-                    src={`${CONFIG.ASSET_IMAGE_URL}frontend/images/emi/emi_cal.jpg`}
-                    alt="about"
-                    className="h-[80vh] object-cover w-[100%]"
-                />
-                <h2 className="xl:text-[18px] text-[16px] midlandfontmedium text-[#143C5E] absolute top-[30%] xl:top-[50%] left-[10%] mb-[0.5rem] font-medium tracking-[5px]">
-                    EMI CALCULATOR
-                </h2>
-                <p className="absolute top-[30%] xl:top-[50%] left-[10%] mt-[2.5rem] text-[#6C6C6C] tracking-[2px] text-[14px]">
-                    HOME - EMI CALCULATOR
-                </p>
-            </div>
-
+            <HeroSectionAboutUs
+                img={`${CONFIG.ASSET_IMAGE_URL}frontend/images/emi/emi_cal.jpg`}
+                heading={"EMI CALCULATOR"}
+                breadCrumb={"HOME - EMI CALCULATOR"}
+            />
             <Index />
         </>
     )
