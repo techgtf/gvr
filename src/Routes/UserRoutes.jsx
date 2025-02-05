a1b9b3334e6b2a11710c1fadf08d5dfe023fff7b
=techgtf <bhupendra.singh@gtftechnologies.com>
2025-02-05 15:59:48 +0530

    residential page created and working on tax benefits

--- src/Routes/UserRoutes.jsx
+++ src/Routes/UserRoutes.jsx
@@ -17,6 +17,8 @@ import ContactUs from "../frontend/pages/Contactus";
 import Csr from "../frontend/pages/Csr";
 import ErrorBoundary from "../frontend/components/ErrorBoundary";
 import AnandamMicrosite from "../frontend/pages/AnandamMicrosite";
+import TaxBenifits from "../frontend/pages/TaxBenifits";
+import NriCorner from "../frontend/pages/NriCorner";
 
 export const UserRoutes = [
   {
@@ -111,6 +113,26 @@ export const UserRoutes = [
           <Suspense fallback={<Loader />}>
             <Layout>
               <Csr />
+            </Layout>
+          </Suspense>
+        ),
+      },
+      {
+        path: `${BASE_ROOT}tax-benefits`,
+        element: (
+          <Suspense fallback={<Loader />}>
+            <Layout>
+              <TaxBenifits />
+            </Layout>
+          </Suspense>
+        ),
+      },
+      {
+        path: `${BASE_ROOT}nri-corner`,
+        element: (
+          <Suspense fallback={<Loader />}>
+            <Layout>
+              <NriCorner />
             </Layout>
           </Suspense>
         ),
