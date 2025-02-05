6d03e3be176700b631d4e244f98dd6fc500183fe
vikas2489 <vk55002678@gmail.com>
2025-02-05 19:01:38 +0530

    update content

--- src/frontend/components/career/JobForm.jsx
+++ src/frontend/components/career/JobForm.jsx
@@ -20,9 +20,7 @@ const JobForm = () => {
           <CommonPera
             PeraClass="fontItalic text-justify xl:text-center xl:!p-[0px] pb-[30px]"
             //   PeraText="The genesis of Great Value Industries dates to 1970 when the group set up its glassware division. In 1990 GVIL diversified into together supplying quality packaging products to prestigious."
-            PeraText={
-              "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-            }
+            PeraText={" Start Your Journey to Excellence Here"}
           />
         </div>
         <form
@@ -76,7 +74,7 @@ const JobForm = () => {
                 htmlFor="designation"
                 className=" mb-[13px] poppins-regular font-[500] block"
               >
-                YOUR DESINATION
+                YOUR DESIGNATION
               </label>
               <input
                 type="text"
