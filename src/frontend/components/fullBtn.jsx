62b07628ad098033fc692f8fa83d18a19c48d16e
=techgtf <bhupendra.singh@gtftechnologies.com>
2025-02-14 14:25:42 +0530

    merged

--- src/frontend/components/fullBtn.jsx
+++ src/frontend/components/fullBtn.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-<<<<<<< HEAD
 export default function FullBtn({ link = "#", text = "Click" }) {
     const isExternal = link.startsWith("http");
 
@@ -26,28 +25,3 @@ export default function FullBtn({ link = "#", text = "Click" }) {
         </Link>
     );
 }
-=======
-export default function FullBtn({
-    link = "",
-    text = "",
-    LinkName = Link, // default    
-}) {
-    const isExternal = LinkName === "a"
-    return (
-        <div className='fullBtn text-white w-fit flex items-center gap-3 lg:py-[10px] lg:px-[25px] px-[18px] py-[7px]'>
-            {
-                isExternal ?
-                    (<a
-                        className="link tracking-[2px] uppercase text-[12px]"
-                        href={link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    > {text || "Click"}</a>) : (
-                        <Link className='link tracking-[2px] uppercase text-[12px]' to={link}>{text || 'click'}</Link>
-                    )
-            }
-            <span className='line inline-block w-[16px] h-[2px] bg-white'></span>
-        </div>
-    )
-}
->>>>>>> origin/bp
