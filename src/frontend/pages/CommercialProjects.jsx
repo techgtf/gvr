02dbfa64011691f6782dd31a5acb66e6d14ee401
vikas2489 <vk55002678@gmail.com>
2025-02-19 10:53:02 +0530

    update commercial logic

--- src/frontend/pages/CommercialProjects.jsx
+++ src/frontend/pages/CommercialProjects.jsx
@@ -385,7 +385,7 @@ const CommercialProjectSection = forwardRef(({ project }, ref) => {
               </p>
             </SlideIn>
           </div>
-          {/* <SlideIn duration={2} delay={0.5}> */}
+
           <div className="mt-[4rem]">
             <h3 className="uppercase text-primary border-b-[1px] border-b-primary pb-[0.4rem] text-[16px]">
               All Projects
@@ -400,34 +400,33 @@ const CommercialProjectSection = forwardRef(({ project }, ref) => {
                   <p className="basis-[30%]">{proj.address}</p>
 
                   <div className="h-[40px] w-[0.5px] bg-[#ddd]"></div>
-
-                  {proj.name == "COMPLEX MADANGIR" ||
-                  proj.name == "JHARKHAND BHAWAN" ||
-                  proj.name == "GAP" ||
-                  proj.name == "PERNIA" ? (
-                    <div className="basis-[25%] ">
-                      {proj.name == "JHARKHAND BHAWAN"
-                        ? "High Street Retail & Office Spaces"
-                        : "Office Spaces"}
-                    </div>
+                  {[
+                    "COMPLEX MADANGIR",
+                    "JHARKHAND BHAWAN",
+                    "GAP",
+                    "PERNIA",
+                  ].includes(proj.name) ? (
+                    proj.name === "JHARKHAND BHAWAN" ? (
+                      <p className="basis-[25%]">
+                        High Street Retail & Office Spaces
+                      </p>
+                    ) : (
+                      <p className="basis-[25%]">Office Spaces</p>
+                    )
                   ) : (
-                    <div className="basis-[25%] ">
-                      <Link
-                        to={proj.link}
-                        className="bg-primary text-[11px] py-[8px] px-[15px] text-white"
-                      >
-                        {proj.name == "Tavru Sohna" ||
-                        proj.name == "Moserbear Part 2"
-                          ? "COMING SOON"
-                          : "READ MORE"}
-                      </Link>
-                    </div>
+                    <Link
+                      to={proj.link}
+                      className="bg-primary text-[11px] py-[8px] px-[15px] text-white"
+                    >
+                      {["Tavru Sohna", "Moserbear Part 2"].includes(proj.name)
+                        ? "COMING SOON"
+                        : "READ MORE"}
+                    </Link>
                   )}
                 </div>
               );
             })}
           </div>
-          {/* </SlideIn> */}
         </div>
       </div>
 
