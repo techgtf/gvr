dace331cc475ba0a60515fd90ce5df0f974fa3b8
vikas2489 <vk55002678@gmail.com>
2025-02-21 15:17:29 +0530

    update animation in commercial

--- src/frontend/pages/PropertyInvestment.jsx
+++ src/frontend/pages/PropertyInvestment.jsx
@@ -2,6 +2,8 @@ import { Helmet } from "react-helmet";
 import * as CONFIG from "../../../config";
 import { lazy } from "react";
 import { useImageReveal } from "../components/useImageReveal";
+import SlideIn from "../components/Animations/SlideIn";
+import { Slide } from "react-toastify";
 
 const HeroSectionAboutUs = lazy(() =>
   import("../components/aboutUs/HeroSectionAboutUs")
@@ -19,6 +21,7 @@ const ProperyInvestment = () => {
           img={`${CONFIG.ASSET_IMAGE_URL}frontend/images/propertyInvestment/property-investment.webp`}
         />
         <div className="flex xl:mt-[4rem] mt-[2rem] flex-wrap justify-between items-center max-w-[90%] xl:max-w-[85%] mx-auto my-0">
+          {" "}
           <div className=" xl:basis-[45%] basis-[100%]">
             <h2 className="sectionHeading xl:mb-[37px] mb-[24px] tracking-[5px] text-black midlandfontmedium text-[14px]">
               Real Estate Investment In India
@@ -44,16 +47,20 @@ const ProperyInvestment = () => {
           />
         </div>
         <div className=" xl:mt-[4rem] mt-[0.5rem] mb-[30px] max-w-[90%] xl:max-w-[85%] mx-auto my-0 ">
-          <h2 className="sectionHeading mb-[37px] tracking-[5px] text-black midlandfontmedium text-[14px] text-center">
-            Real Estate Investment In India
-          </h2>
+          <SlideIn duration={2} delay={0.5}>
+            <h2 className="sectionHeading mb-[37px] tracking-[5px] text-black midlandfontmedium text-[14px] text-center">
+              Real Estate Investment In India
+            </h2>
+          </SlideIn>
           <div className="relative">
-            <p className="absolute top-[30px] xl:left-[7.5%] left-[8%] bg-[#EFF5FA] xl:px-[60px] px-[30px] py-[20px] w-[85%] text-center tracking-[4px] font-[300]">
-              The Delhi National Capital Region (NCR) has been a focal point for
-              real estate investments, with Noida standing out as a prime
-              destination due to its strategic location, infrastructure
-              development, and economic opportunities.
-            </p>
+            <SlideIn duration={2} delay={0.5}>
+              <p className="absolute top-[30px] xl:left-[7.5%] left-[8%] bg-[#EFF5FA] xl:px-[60px] px-[30px] py-[20px] w-[85%] text-center tracking-[4px] font-[300]">
+                The Delhi National Capital Region (NCR) has been a focal point
+                for real estate investments, with Noida standing out as a prime
+                destination due to its strategic location, infrastructure
+                development, and economic opportunities.
+              </p>
+            </SlideIn>
             <img
               // src={`${CONFIG.ASSET_IMAGE_URL}frontend/images/propertyInvestment/real_estate.webp`}
               src={
@@ -66,17 +73,19 @@ const ProperyInvestment = () => {
         </div>
         <div className="flex xl:mt-[4rem] mb-[37px] mt-[2rem] justify-between items-center max-w-[90%] flex-wrap xl:max-w-[85%] mx-auto my-0">
           <div className="xl:basis-[45%] basis-[100%]">
-            <h2 className="sectionHeading xl:mb-[37px] mb-[24px] tracking-[5px] text-black midlandfontmedium text-[14px]">
-              Real Estate Investment In India
-            </h2>
-            <p className="text-[#666] text-justify">
-              Noida has experienced a substantial increase in housing prices.
-              Data from Anarock indicates that average housing prices along the
-              Noida Expressway rose by 66% over the past five years, reaching Rs
-              8,400 per square foot as of September 2024. This surge is
-              attributed to enhanced connectivity and the establishment of
-              commercial hubs in the vicinity.
-            </p>
+            <SlideIn duration={2} delay={0.5}>
+              <h2 className="sectionHeading xl:mb-[37px] mb-[24px] tracking-[5px] text-black midlandfontmedium text-[14px]">
+                Real Estate Investment In India
+              </h2>
+              <p className="text-[#666] text-justify">
+                Noida has experienced a substantial increase in housing prices.
+                Data from Anarock indicates that average housing prices along
+                the Noida Expressway rose by 66% over the past five years,
+                reaching Rs 8,400 per square foot as of September 2024. This
+                surge is attributed to enhanced connectivity and the
+                establishment of commercial hubs in the vicinity.
+              </p>
+            </SlideIn>
           </div>
           <img
             className="basis-[45%] reveal w-[400px] mt-[37px] xl:mt-0"
@@ -97,30 +106,35 @@ const ProperyInvestment = () => {
             alt="group"
           />
           <div className="xl:basis-[45%] basis-[100%]">
-            <h2 className="sectionHeading mb-[37px] tracking-[5px] text-black midlandfontmedium text-[14px]">
-              Luxury Segment Expansion
-            </h2>
-            <p className="text-[#666] text-justify">
-              The luxury housing segment in Noida has seen remarkable growth.
-              For instance, Sector 150 witnessed a 12% year-on-year increase in
-              monthly rentals for luxury properties in the first half of 2024.
-              This trend reflects the rising demand for premium residences
-              equipped with modern amenities.
-            </p>
+            <SlideIn duration={2} delay={0.5}>
+              {" "}
+              <h2 className="sectionHeading mb-[37px] tracking-[5px] text-black midlandfontmedium text-[14px]">
+                Luxury Segment Expansion
+              </h2>
+              <p className="text-[#666] text-justify">
+                The luxury housing segment in Noida has seen remarkable growth.
+                For instance, Sector 150 witnessed a 12% year-on-year increase
+                in monthly rentals for luxury properties in the first half of
+                2024. This trend reflects the rising demand for premium
+                residences equipped with modern amenities.
+              </p>
+            </SlideIn>
           </div>
         </div>
         <div className="flex xl:mt-[4rem] mt-[2rem] justify-between items-center max-w-[90%] xl:max-w-[85%] mx-auto my-0 flex-wrap">
           <div className="xl:basis-[45%] basis-[100%]">
-            <h2 className="sectionHeading mb-[37px] tracking-[5px] text-black midlandfontmedium text-[14px]">
-              Infrastructure Development
-            </h2>
-            <p className="text-[#666] text-justify">
-              Ongoing infrastructure projects, such as the development of the
-              Noida International Airport and the expansion of metro
-              connectivity, have significantly bolstered Noida's real estate
-              appeal. These developments are anticipated to further drive
-              property values and attract both investors and end-users.
-            </p>
+            <SlideIn duration={2} delay={0.5}>
+              <h2 className="sectionHeading mb-[37px] tracking-[5px] text-black midlandfontmedium text-[14px]">
+                Infrastructure Development
+              </h2>
+              <p className="text-[#666] text-justify">
+                Ongoing infrastructure projects, such as the development of the
+                Noida International Airport and the expansion of metro
+                connectivity, have significantly bolstered Noida's real estate
+                appeal. These developments are anticipated to further drive
+                property values and attract both investors and end-users.
+              </p>
+            </SlideIn>
           </div>
           <img
             className="basis-[45%] reveal w-[400px] xl:mt-[0px] mt-[27px]"
@@ -132,19 +146,22 @@ const ProperyInvestment = () => {
           />
         </div>
         <div className=" xl:mt-[4rem] mt-[2rem] pb-[5rem] max-w-[90%] xl:max-w-[85%] mx-auto my-0">
-          <h3 className="sectionHeading mb-[37px] mt-[40px] tracking-[5px] text-black midlandfontmedium text-[14px]">
-            Conclusion
-          </h3>
-          <p className="text-[#666] text-justify">
-            The upward trajectory of India's real estate sector, particularly in
-            regions like Noida within the Delhi NCR, presents lucrative
-            opportunities for investors. Factors such as escalating property
-            prices, burgeoning demand in the luxury segment, and continuous
-            infrastructure advancements contribute to Noida's prominence as a
-            preferred investment hub. As the market evolves, stakeholders are
-            advised to stay informed and consider strategic investments to
-            capitalize on the region's growth potential.
-          </p>
+          <SlideIn duration={2} delay={0.5}>
+            {" "}
+            <h3 className="sectionHeading mb-[37px] mt-[40px] tracking-[5px] text-black midlandfontmedium text-[14px]">
+              Conclusion
+            </h3>
+            <p className="text-[#666] text-justify">
+              The upward trajectory of India's real estate sector, particularly
+              in regions like Noida within the Delhi NCR, presents lucrative
+              opportunities for investors. Factors such as escalating property
+              prices, burgeoning demand in the luxury segment, and continuous
+              infrastructure advancements contribute to Noida's prominence as a
+              preferred investment hub. As the market evolves, stakeholders are
+              advised to stay informed and consider strategic investments to
+              capitalize on the region's growth potential.
+            </p>
+          </SlideIn>
         </div>
       </section>
     </>
