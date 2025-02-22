d97f5bfe1979117e56e5e4ad0832de8078a516df
=techgtf <bhupendra.singh@gtftechnologies.com>
2025-02-22 14:38:12 +0530

    home page meta desc updated, heatmap added in index.html

--- src/frontend/pages/Home.jsx
+++ src/frontend/pages/Home.jsx
@@ -11,25 +11,12 @@ import { Helmet } from 'react-helmet'
 
 export default function Home() {
 
-    // const arr = [12, 5, 7, 7, 6, 4, 58, 8];
-
-    // function giveUniqNumber(arr) {
-    //     if (arr.length < 0) return;
-    //     let uniqeArr = arr[0]
-    //     for (let i = 0; i < arr.length; i++) {
-    //         if (arr[i] === uniqeArr) { 
-    //             uniqeArr
-    //         }
-    //     }
-    // }
-
-
     return (
         <>
             <Helmet>
                 <title> Great Value Realty | Your Trusted Real Estate Partner</title>
                 <meta name="keywords" content="Great Value realty, Great Value Sharanam Noida, Great Value Anandam Noida, Sector 107 Noida,  Great Value Anandam, Great Value Sharanam Sector 107 Noida, Best Casa Uday Delhi, Great Value Realty Vilasa, Best Sanctuary residential project sector 105 " />
-                <meta name="description" content="At Great Value Realty, we go beyond building homes—we build trust, deliver exceptional value, and create lasting experiences." />
+                <meta name="description" content="Great Value Realty brings you luxurious & affordable homes in NCR, Noida & Gurugram. Experience unmatched quality & trust in real estate." />
                 <meta name="google-site-verification" content="Ma-arPYmEe7u20NJ-jsuiHjD1p2HSShiEPD4m8s3bL8" />
                 <meta name="distribution" content="Global" />
                 <meta name="Language" content="English" />
